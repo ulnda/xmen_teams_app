f1118f254c47007952c5ba598dc731b4f40aaa01
ulnda <denis.nagibin@gmail.com>
2015-09-24 14:51:31 +0300

    New mutant creating

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -2,4 +2,22 @@ class MutantsController < ApplicationController
   def index
     @mutants = Mutant.all
   end
+
+  def new
+    @mutant = Mutant.new
+  end
+
+  def create
+    @mutant = Mutant.new(mutant_params)
+    if @mutant.save
+      redirect_to mutants_path
+    else
+      render 'new'
+    end 
+  end
+
+  private
+    def mutant_params
+      params.require(:mutant).permit(:name, :description)
+    end
 end
